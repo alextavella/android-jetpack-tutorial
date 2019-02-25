fa9b06eb143b9bbc565efb2063644ac19c5786d6
alex.tavella <alex.tavella@viavarejo.com.br>
2019-02-25 12:00:27 -0300

    update setup on start fragment

--- app/src/main/java/com/example/androidjetpacktutorial/ui/start/StartFragment.kt
+++ app/src/main/java/com/example/androidjetpacktutorial/ui/start/StartFragment.kt
@@ -37,9 +37,7 @@ class StartFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.start_fragment, container, false)
-    }
+    ): View = inflater.inflate(R.layout.start_fragment, container, false)
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -49,8 +47,8 @@ class StartFragment : Fragment() {
         })
     }
 
-    override fun onResume() {
-        super.onResume()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         buttonCounter.setOnClickListener { viewModel.increaseCount() }
     }
